c8914a384baf0e56edca233cc0d82dc7058b7603
Camilo-J <josecamilo1902@outlook.com>
2023-03-07 14:15:07 -0500

    Add curd methods for taskController

---
+++ app/controllers/task.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import { Project } from "../models/Project";
+import { Task } from "../models/Task";
+
+const getTasks = async (req: Request, res: Response) => {
+  const projects = await Project.findAll();
+  res.json(projects);
+};
+
+const createTask = async (req: Request, res: Response) => {
+  const data = req.body;
+  const task = await Task.create({ ...data });
+  res.json(task);
+};
+const updateTask = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = req.body;
+  const task = await Task.findOne({
+    where: {
+      id,
+    },
+  });
+  task?.update({
+    ...data,
+  });
+  await task?.save();
+
+  res.json(task);
+};
+const destroyTask = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = req.body;
+  const task = await Task.findOne({
+    where: {
+      id,
+    },
+  });
+  task?.destroy({
+    ...data,
+  });
+  if (!task) res.status(404).json({ message: "Task not found" });
+
+  res.sendStatus(204);
+};
+
+export { getTasks, createTask, updateTask, destroyTask };
