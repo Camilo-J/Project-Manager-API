a13ef0dc8add6a5960225bc72f788c368bd38a32
Camilo-J <josecamilo1902@outlook.com>
2023-03-08 17:23:37 -0500

    Add config to init server

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,15 @@
 import express from "express";
+import cors from "cors";
+import "dotenv";
+import { router } from "./routes";
 
 const app = express();
+const PORT = process.env.PORT || 3002;
 
-app.listen(3000);
-console.log("Server on port ", 3000);
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", router);
+
+app.listen(PORT);
+console.log("Server on port ", PORT);
